fead000c8ba747d877f04d6808b7d209644fd903
Sinavorn <sinavorn670@gmail.com>
2025-07-19 21:57:59 +0700

    model/comment.js

--- BackEnd/src/models/comments.js
+++ BackEnd/src/models/comments.js
@@ -1,5 +1,40 @@
-class Comments extends Model {}
+    import { Model, DataTypes } from 'sequelize';
+    import sequelize from '../config/database.js';
+    import User from './user.js'; // Adjust path as needed
+    import Post from './post.js'; // Adjust path as needed
 
-Comments.init();
+    class Comments extends Model {}
 
-export default Comments;
+    Comments.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            text: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            userId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            postId: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'Comments',
+            tableName: 'comments',
+            timestamps: true,
+        }
+    );
+
+    // Associations
+    Comments.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+    Comments.belongsTo(Post, { foreignKey: 'postId', as: 'post' });
+
+    export default Comments;
