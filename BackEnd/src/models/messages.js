63e0ddc7e67332b6b62f6a02f5a7dcfd98e6499c
jessica2882 <kimj22676@gmail.com>
2025-07-21 00:01:14 +0700

    Update messages.js

--- BackEnd/src/models/messages.js
+++ BackEnd/src/models/messages.js
@@ -1,5 +1,41 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../db/database.js';
 class Message extends Model {}
 
-Message.init();
+Message.init(
+  {
+ 
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    senderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    receiverId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    sentAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW, 
+    },
+  },
+  {
+    sequelize,            
+    modelName: 'Message', 
+    tableName: 'messages',
+    timestamps: false,    
+  }
+);
+
 
 export default Message;
